77a4d73e9aab568728fb32dbf88337012b44284b
Esmeralda Torres Aguirre <esmeraldatorresaguirre@MacBook-Air-de-Esmeralda.local>
2024-05-21 18:54:49 -0600

    first commit

---
+++ src/routes/user.router.js
@@ -0,0 +1,30 @@
+const {
+  getAll,
+  create,
+  getOne,
+  remove,
+  update,
+  verifyCode,
+  login,
+  getLoggedUser,
+} = require("../controllers/user.controller");
+const express = require("express");
+const verifyJWT = require("../utils/verifiJWT");
+
+const userRouter = express.Router();
+
+userRouter.route("/users").get(verifyJWT, getAll).post(create);
+
+userRouter.route("/users/login").post(login);
+
+userRouter.route("/users/me").get(verifyJWT, getLoggedUser);
+
+userRouter
+  .route("/users/:id")
+  .get(verifyJWT, getOne)
+  .delete(verifyJWT, remove)
+  .put(verifyJWT, update);
+
+userRouter.route("/users/verify/:code").get(verifyCode);
+
+module.exports = userRouter;
